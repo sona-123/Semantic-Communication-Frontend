d956f21580495afe2380692da8c97ef8586533e0
Sonali Gupta <sonalibyahut0125@gmail.com>
2025-05-15 21:31:59 +0530

    Added Semantic Communication - Backend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <OneView/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OneView from './layout/OneView.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    OneView,
+  },
+
+  setup() {
+    return {
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+const pinia = createPinia()
+loadFonts()
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .use(pinia)
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+    position: "top-right",
+  })
+  .mount('#app')
